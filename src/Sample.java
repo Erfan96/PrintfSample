c0d2a63cc9bab5116fc561dc84e8cf3d7d4742f3
efsam <E.salimi75@ymail.com>
2020-11-22 18:45:00 +0330

    Final commit to Secondary

--- src/Sample.java
+++ src/Sample.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 public class Sample {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.println("================================");
         for (int i=0 ; i<3 ; i++) {
             String str = s.next();
             int number = s.nextInt();
 
-            System.out.printf("%-15s%3d%n", str, number);
+            if (str.length()<11 && number<1000) {
+                System.out.printf("%-15s%03d%n", str, number);
+            }
         }
+
+        System.out.println("================================");
     }
 }
