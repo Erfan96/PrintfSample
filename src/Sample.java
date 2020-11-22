1255d845b1842220745a3d92fde64bc997f2eb51
efsam <E.salimi75@ymail.com>
2020-11-22 14:55:23 +0330

    commit for save changes in Secondary

--- src/Sample.java
+++ src/Sample.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class Sample {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        for (int i=0 ; i<3 ; i++) {
+            String str = s.next();
+            int number = s.nextInt();
+
+            System.out.printf("%-15s%3d%n", str, number);
+        }
+    }
 }
